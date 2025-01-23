91391e7cc595a0333139d4876bacbc171c2a93dc
Luisa Alejandra <luisaalejandrauicabcetz@gmail.com>
2025-01-23 16:42:37 -0600

    props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Header from "./Header";
 
 function App() {
+  const myCheese = {
+    name: "Camember",
+    smellFactor: "Extreme pong",
+    price: 3.5,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hola a todos</p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <TodoComponent mssg="I like cheese" cheese={myCheese} />
+    </>
   );
 }
 
+class TodoComponent extends Component {
+  render() {
+    return (
+      <>
+        <p>
+          <strong>{this.props.mssg}</strong>
+        </p>
+        <p>
+          <strong>Cheese name:</strong> {this.props.cheese.name}
+        </p>
+        <p>
+          <strong>Cheese smell factor:</strong> {this.props.cheese.smellFactor}
+        </p>
+        <p>
+          <strong>Cheese price:</strong> {this.props.cheese.price}
+        </p>
+      </>
+    );
+  }
+}
 export default App;
