819d50c318bf98cabdfd2a73e1c24aa7de1420eb
Luisa Alejandra <luisaalejandrauicabcetz@gmail.com>
2025-01-23 18:05:34 -0600

    Cycling Through Data

--- src/App.js
+++ src/App.js
@@ -15,31 +15,37 @@ class TodoComponent extends Component {
   constructor() {
     super();
     this.state = {
-      todos: ["wash up", "eat some cheese", "take a nap"],
-      age: 30
+      todos: ["wash up", "eat some cheese", "take a nap", "wake"],
+      age: 30,
     };
   }
 
   componentDidMount() {
     setTimeout(() => {
-      this.setState({age: 35})
+      this.setState({ age: 35 });
     }, 5000);
   }
 
+  // todoList = () => {
+  //   return this.state.todos.map((todo, index) => (
+  //     <li className="list-group-item">{todo}</li>
+  //   ))
+  // }
+
   render() {
+    const todoList = this.state.todos.map((item, index) => (
+      <li key={index} className="list-group-item">{item}</li>
+    ));
+
     return (
       <>
         <div className="container">
           <p>The busiest people have the most leisure...</p>
           <p>{this.state.age}</p>
-          <ul className="list-group">
-            <li className="list-group-item">{this.state.todos[0]}</li>
-            <li className="list-group-item">{this.state.todos[1]}</li>
-            <li className="list-group-item">{this.state.todos[2]}</li>
-          </ul>
+          <ul className="list-group">{todoList}</ul>
         </div>
       </>
-    )
+    );
   }
 }
 export default App;
