bd1eb6dfb06475a2b13ba829cdc4ddd560ceeaf6
Luisa Alejandra <luisaalejandrauicabcetz@gmail.com>
2025-01-24 10:46:29 -0600

    Handling Events - Delete

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ class TodoComponent extends Component {
   constructor() {
     super();
     this.state = {
-      todos: ["wash up", "eat some cheese", "take a nap", "wake"],
+      todos: ["wash up", "eat some cheese", "take a nap", "take a shower"],
       age: 30,
     };
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleDelete = this.handleDelete.bind(this)
   }
 
   componentDidMount() {
@@ -32,15 +34,27 @@ class TodoComponent extends Component {
   //   ))
   // }
 
+  handleOnClick() {
+    alert(`you clicked me`);
+  }
+
+  handleDelete(item) {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((val, index) => item !== val),
+    }));
+  }
+
   render() {
     const todoList = this.state.todos.map((item, index) => (
-        <TodoItem item={item} key={index} />
+      <TodoItem item={item} key={index} handleDelete={this.handleDelete} />
     ));
 
     return (
       <>
         <div className="container">
-          <p>The busiest people have the most leisure...</p>
+          <p onClick={this.handleOnClick}>
+            The busiest people have the most leisure...
+          </p>
           <p>{this.state.age}</p>
           <ul className="list-group">{todoList}</ul>
         </div>
@@ -51,11 +65,20 @@ class TodoComponent extends Component {
 
 class TodoItem extends Component {
   render() {
-    return(
-      <>
-        <li key={this.props.index} className="list-group-item">{this.props.item}</li>
-      </>
-    )
+    return (
+      <li
+        key={this.props.index}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        {this.props.item}
+        <button
+          type="button"
+          className="btn-close"
+          arial-label="Close"
+          onClick={() => this.props.handleDelete(this.props.item)}
+        ></button>
+      </li>
+    );
   }
 }
 
